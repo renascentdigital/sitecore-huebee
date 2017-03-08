4276372150a00b73589600cd3d725b28dc4ced27
Ehren Dames <ehren@ehrendames.com>
2017-03-07 19:14:19 -0700

    chore: add css/style loaders

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ module.exports = {
                     options: { presets: ['es2015'] }
                 }],
             },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
         ]
     }
 };
