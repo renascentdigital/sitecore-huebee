08b36d7b0fcc4980ff99807d49df758ea5a72888
Ehren Dames <ehren@ehrendames.com>
2017-03-07 19:16:56 -0700

    feat: add picker class
    Add class for Picker
    Initialize Huebee with constructor

---
+++ src/js/picker.js
@@ -0,0 +1,10 @@
+import styles from 'huebee/dist/huebee.css';
+let Huebee = require('huebee/dist/huebee.pkgd.js');
+
+class Picker {
+  constructor(container) {
+      let hueb = new Huebee(container, {});
+  }
+}
+
+export default Picker;
