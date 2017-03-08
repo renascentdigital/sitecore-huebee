a1a28587fc9992f66d494d8a0a0d77a33e6e8171
Ehren Dames <ehren@ehrendames.com>
2017-03-07 19:38:26 -0700

    feat: add render content editor pipeline
    Add pipeline to load assets for color picker

---
+++ src/Pipelines/RenderContentEditor/LoadContentEditorAssets.cs
@@ -0,0 +1,28 @@
+﻿namespace Sitecore.Huebee.Pipelines.RenderContentEditor
+{
+    using Sitecore.Diagnostics;
+    using Sitecore.Pipelines;
+    using System.Web;
+    using System.Web.UI;
+
+    public class LoadContentEditorAssets
+    {
+        public void Process(PipelineArgs args)
+        {
+            if(Context.ClientPage.IsEvent)
+                return;            
+
+            HttpContext current = HttpContext.Current;
+            if (current == null)
+                return;
+
+            Page handler = current.Handler as Page;
+            if (handler == null)
+                return;
+
+            Assert.IsNotNull(handler.Header, "Content Editor <head> tag is missing runat='value'");
+            
+            handler.Header.Controls.Add(new LiteralControl("<script src=\"/sitecore modules/shell/SitecoreHuebee/picker.js\"></script>\n"));
+        }
+    }
+}
