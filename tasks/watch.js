60a529a5ac1f821d1cb57f2a9e95e97b4855731b
Ehren Dames <ehren@ehrendames.com>
2018-03-17 21:28:44 -0600

    chore: add watcher for rapid development

--- tasks/watch.js
+++ tasks/watch.js
@@ -1,7 +1,11 @@
 "use strict";
 const chokidar = require("chokidar");
+const shell = require( "shelljs");
+const path = require( "path");
+const exit = require( "exit-hook");
+const pkg = require('package');
 
-const watcher = chokidar.watch(['src/js'], {
+const watcher = chokidar.watch(['src/js', 'src/**/*.cs'], {
     ignored: /[/\\]\./,
     ignoreInitial: true
 });
@@ -13,6 +17,16 @@ const checkTypeOfEvent = (f) => {
     const type = path.extname(f).split('.')[1];
 
     if(type === 'js') {}
+    if(type === 'cs') {
+        buildModule();
+    }
+}
+
+const buildModule = () => {
+  console.log(pkg.config.unicorn);
+  shell.exec('msbuild src/Sitecore.Huebee.csproj /p:Configuration=Debug', function(code, output){
+    console.log('Building Sitecore Module!!!');
+  });
 }
 
 exit(function() {
