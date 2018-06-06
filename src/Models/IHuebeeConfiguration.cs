88903060122500ad1ff68956eefa4eb1bdeeeae6
Ehren Dames <ehren@ehrendames.com>
2018-06-06 08:27:32 -0600

    feat(Configuration): add Huebee configuration
    Add models to represent configuration for module
    Add factory for getting configuration
    Add dependency to Color Picker Field class
    
    References #3

---
+++ src/Models/IHuebeeConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sitecore.Huebee.Models
+{
+    public interface IHuebeeConfiguration
+    {
+        string Hues { get; set; }
+        string Hue0 { get; set; }
+        string Shades { get; set; }
+        string Saturations { get; set; }
+        string Notation { get; set; }
+        IEnumerable<CustomColor> CustomColors { get; }
+    }
+}
