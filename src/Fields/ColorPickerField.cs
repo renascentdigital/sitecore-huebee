9d9d199d0d6d906d04811cb029b6e85a9aeb1d16
Ehren Dames <ehren@ehrendames.com>
2018-09-22 15:09:13 -0600

    feat(Configuration): add method to generate options from configuration
    Closes #3

--- src/Fields/ColorPickerField.cs
+++ src/Fields/ColorPickerField.cs
@@ -5,6 +5,7 @@ using Sitecore.Shell.Applications.ContentEditor;
 using Sitecore.Web.UI.HtmlControls;
 using Sitecore.Web.UI.Sheer;
 using System;
+using System.Linq;
 using System.Web.UI;
 
 namespace Sitecore.Huebee.Fields
@@ -33,10 +34,11 @@ namespace Sitecore.Huebee.Fields
 
         protected override void Render(HtmlTextWriter output)
         {
+            string options = CompileConfiguration();
             output.Write("<div class=\"o-colorpicker\">");
             base.Render(output);
             output.Write("</div>");
-            output.Write("<script>var huebeeColorInput = document.querySelector('.o-colorpicker'); if(huebeeColorInput && huebeeColorInput.firstChild) { var huebeePicker = new SitecoreHuebee.Picker(huebeeColorInput.firstChild, { staticOpen: true }); }</script>");
+            output.Write("<script>var huebeeColorInput = document.querySelector('.o-colorpicker'); if(huebeeColorInput && huebeeColorInput.firstChild) { var huebeePicker = new SitecoreHuebee.Picker(huebeeColorInput.firstChild, { " + options + ", \"staticOpen\": true }); }</script>");
         }
 
         public override void HandleMessage(Message message)
@@ -56,5 +58,43 @@ namespace Sitecore.Huebee.Fields
             SetValue(string.Empty);
             SheerResponse.Eval("huebeePicker.clear('" + this.ClientID + "');");
         }        
+
+        private string CompileConfiguration()
+        {
+            string options = string.Empty;
+            if(!string.IsNullOrWhiteSpace(_configuration.Notation))
+            {
+                options += $"\"notation\": '{_configuration.Notation}',";
+            }
+            if(!string.IsNullOrWhiteSpace(_configuration.Hue0))
+            {
+                options += $"\"hue0\": {_configuration.Hue0},";
+            }
+            if(!string.IsNullOrWhiteSpace(_configuration.Hues))
+            {
+                options += $"\"hues\": {_configuration.Hues},";
+            }
+            if(!string.IsNullOrWhiteSpace(_configuration.Saturations))
+            {
+                options += $"\"saturations\": {_configuration.Saturations},";
+            }
+            if(!string.IsNullOrWhiteSpace(_configuration.Shades))
+            {
+                options += $"\"shades\": {_configuration.Shades},";
+            }
+
+            string customColors = string.Empty;
+            if(_configuration.CustomColors != null && _configuration.CustomColors.Any())
+            {
+                foreach (CustomColor color in _configuration.CustomColors)
+                {
+                    customColors += $"'{color.Code}',";
+                }
+                if(customColors.Length > 0)
+                    options += $"\"customColors\": [{customColors.Substring(0,customColors.Count() - 1)}],";
+            }
+            options = options.Substring(0, options.Count() - 1);
+            return options;
+        }
     }
 }
