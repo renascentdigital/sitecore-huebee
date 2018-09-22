1ce492da950fb56fa1834dd9f54dba754e1d2a5d
Ehren Dames <ehren@ehrendames.com>
2018-09-22 15:09:13 -0600

    feat(Configuration): add method to generate options from configuration
    Closes #3

--- src/Models/HuebeeConfiguration.cs
+++ src/Models/HuebeeConfiguration.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Xml;
+using System.Linq;
 
 namespace Sitecore.Huebee.Models
 {
@@ -16,6 +17,19 @@ namespace Sitecore.Huebee.Models
         public string Saturations { get; set; }
         public string Notation { get; set; }
         public IEnumerable<CustomColor> CustomColors => _customColors;
+
+        public void AddOptions(XmlNode node)
+        {
+            if(node == null)
+            {
+                Sitecore.Diagnostics.Log.SingleWarn("sitecore Huebee configuration not is not defined", this);
+            }
+            Hues = (node.SelectSingleNode("hues") != null) ? node.SelectSingleNode("hues").Value : string.Empty;
+            Hue0 = (node.SelectSingleNode("hue0") != null) ? node.SelectSingleNode("hue0").Value : string.Empty;
+            Shades = (node.SelectSingleNode("shades") != null) ? node.SelectSingleNode("shades").Value : string.Empty;
+            Saturations = (node.SelectSingleNode("saturations") != null) ? node.SelectSingleNode("saturations").Value : string.Empty;
+            Notation = (node.SelectSingleNode("notation") != null) ? node.SelectSingleNode("notation").Value : string.Empty;
+        }
 
         protected void AddCustomColors(XmlNode node)
         {
