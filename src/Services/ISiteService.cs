fec09e47e4e71a24b5bb68bcafe80a7d4b62b677
Ehren Dames <ehren@ehrendames.com>
2018-09-23 23:11:54 -0600

    feat(Sites): add configuration of color picker per site
    Add configuration to allow the feature to be enabled or disabled
    Add logic to determine site and load appropriate configuration for color picker
    
    Closes #4

---
+++ src/Services/ISiteService.cs
@@ -0,0 +1,11 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Web;
+using System.Collections.Generic;
+
+namespace Sitecore.Huebee.Services
+{
+    public interface ISiteService
+    {
+        IEnumerable<SiteInfo> DetermineSite(Item contextItem);
+    }
+}
---
+++ src/Services/SiteService.cs
@@ -0,0 +1,36 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Web;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sitecore.Huebee.Services
+{
+    public class SiteService : ISiteService
+    {
+        public IEnumerable<SiteInfo> DetermineSite(Item contextItem)
+        {
+            var sites = Sitecore.Configuration.Factory.GetSiteInfoList();
+            var database = Sitecore.Context.ContentDatabase;
+
+            foreach (var site in sites)
+            {
+                var homeItemPath = $"{site.RootPath}{site.StartItem}";
+                var homeItem = database.GetItem(homeItemPath);
+
+                if (homeItem == null)
+                    continue;
+
+                if (homeItem.ID.Equals(contextItem.ID))
+                {
+                    yield return site;
+                }
+                else
+                {
+                    var filterItem = database.SelectItems($"fast:{homeItemPath}//*[@@ID = '{contextItem.ID}']");
+                    if (filterItem.Any())
+                        yield return site;
+                }
+            }
+        }
+    }
+}
